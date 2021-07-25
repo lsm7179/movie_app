4217752d016a661c88d9601d27865731b9bd2e29
이승민 <sml7179@naver.com>
2021-07-25 23:51:26 +0900

    영화 데이터 화면에 그리기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies:[],
+  };
+  getMovies = async() =>{
+    const {
+      data :{
+        data :{
+          movies
+        },
+      },
+    }= await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.setState({movies,isLoading:false});
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const{isLoading}=this.state;
+    return <div>{isLoading ? 'Loading...':'We are ready'}</div>;
+  }
 }
 
 export default App;
